24a835274b5ddec97f01f1ca2609ffd2a25c9e73
cherydactyl <cheryl.orosz@gmail.com>
2015-07-10 14:38:46 -0400

    Basic Structure complete
    a simple and liberal approval model

--- ExpITFunctionsLab/Program.cs
+++ ExpITFunctionsLab/Program.cs
@@ -26,19 +26,61 @@ namespace ExpITFunctionsLab
 
         static bool firstLevelMgr(string description, double price)
         {
-            //check i the decription contains entertainment (regardless of case)
+            //check if the decription contains entertainment (regardless of case)
             if (description.ToLower().Contains("entertainment"))
             {
                 return false;
             }
+            //cannot approve if over $250
             if (price > 250)
             {
                 return secondLevelMgr(description, price);
             }
 
+            //otherwise approve!  we are liberal managers
             return true;
         }
 
+        static bool secondLevelMgr(string description, double price)
+        {
+            //check if the decription contains towncar(s) (regardless of case)
+            if (description.ToLower().Contains("towncar"))
+            {
+                return false;
+            }
+            //cannot approve if over 500, kick it up a level!
+            if (price > 500)
+            {
+               return director(description, price);
+            }
+
+            //otherwise approve!  we are liberal managers
+            return true;
+        }
+
+        static bool director(string description, double price)
+        {
+            //check if the decription contains hardawre (regardless of case)
+            //currently assuming that the keyword hardware will be explicitly contained in the decription
+            if (description.ToLower().Contains("hardware") && price > 5000)
+            {
+                return false;
+            }
+            //cannot approve if over 1000, kick it up a level!
+            if (price > 1000)
+            {
+                return ceo(description, price);
+            }
+
+            //otherwise approve!  we are liberal managers
+            return true;
+        }
+
+        static bool ceo(string description, double price)
+        {
+            //our CEO loves us and wants us to be happy!
+            return true;
+        }
 
     }
 }
